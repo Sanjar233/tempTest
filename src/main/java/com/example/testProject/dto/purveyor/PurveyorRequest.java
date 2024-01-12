a620a98ec8412ad1d5e64a04cd4f23c187c3faf2
Sanjar233 <minbsanj900@gmail.com>
2024-01-12 17:43:55 +0600

    "-"

---
+++ src/main/java/com/example/testProject/dto/purveyor/PurveyorRequest.java
@@ -0,0 +1,14 @@
+package com.example.testProject.dto.purveyor;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PurveyorRequest {
+    private String name;
+    private String address;
+    private String phone_number;
+}
+
+
